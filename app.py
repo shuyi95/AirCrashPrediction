a4a50e3cc1aa98b45ad3522a7fb773b29531fb93
shuyi95 <112841530+shuyi95@users.noreply.github.com>
2024-09-06 20:17:46 -0700

    Add .gitignore to exclude venv

--- app.py
+++ app.py
@@ -2,33 +2,54 @@ from flask import Flask, request, jsonify
 import joblib
 import pandas as pd
 import re
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+import string
+
+# Download NLTK stopwords
+nltk.download('stopwords')
 
 app = Flask(__name__)
 
 # Load the saved SVC model
 svc_model = joblib.load('svc_model.pkl')
 
-# Define preprocessing function
+# Initialize stopwords and stemmer
+stop_words = set(stopwords.words('english'))
+stemmer = PorterStemmer()
+
+# Define the preprocessing
 def preprocess_text(text):
-    # preprocessing
+    # Removing all punctuation
+    text = text.translate(str.maketrans('', '', string.punctuation))
+    # Converting all text to lowercase
     text = text.lower()
-    text = re.sub(r'\W', ' ', text)
+    # Removing stopwords from text
+    text = ' '.join([word for word in text.split() if word not in stop_words])
+    # Apply stemming
+    text = ' '.join([stemmer.stem(word) for word in text.split()])
     return text
+
+@app.route('/')
+def home():
+    return "Welcome to the AirCrashPrediction API"
 
 @app.route('/predict', methods=['POST'])
 def predict():
     data = request.get_json()
     
-    # Preprocess the 'Report' text
-    report = preprocess_text(data['Report'])
+    # Preprocess the 'Report' and 'Part Failure' text
+    report = preprocess_text(data['Report'].strip().lower())
+    part_failure = preprocess_text(data['Part Failure'].strip().lower())
     
-    # Combine the preprocessed text with 'Part Failure'
+    # Combine the preprocessed text for prediction
     input_data = pd.DataFrame({
         'Report': [report],
-        'Part Failure': [data['Part Failure']]
+        'Part Failure': [part_failure]
     })
     
-    # Make prediction
+    # prediction
     prediction = svc_model.predict(input_data)
     return jsonify({'prediction': prediction.tolist()})
 
